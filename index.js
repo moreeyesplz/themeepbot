7ddfd1e17ec510b5b422c40d7fa66ab29d40b58c
Jeremy Ong <jeremycong@gmail.com>
2020-08-17 20:56:19 -0600

    Add missing @actions/github dependency

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const github = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
 const { createAppAuth } = require('@octokit/auth-app');
 
